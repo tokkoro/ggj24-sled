a69081bbb119857bbe2603da898fa4cc48103c58
Sami Jokela <sami.jokela@outlook.com>
2024-01-27 20:45:45 +0200

    add start sounds

--- godot_project/tomi_testaa/CountDownLabels.gd
+++ godot_project/tomi_testaa/CountDownLabels.gd
@@ -7,6 +7,11 @@ class_name CountDownLabel
 @onready var obj_go = $go
 
 func set_label(num: int):
+	if num == 3 || num == 2 || num == 1:
+		$Ping.play()
+	if num == 0:
+		$Ping.pitch_scale = 1.5
+		$Ping.play()
 	obj_go.visible = num == 0
 	obj1.visible = num == 1
 	obj2.visible = num == 2
