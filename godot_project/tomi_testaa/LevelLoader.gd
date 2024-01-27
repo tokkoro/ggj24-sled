db789a0aa0629c641aec21caecd8866322581170
Raattis <raattis@gmail.com>
2024-01-28 01:50:33 +0200

    Load props for levels

--- godot_project/tomi_testaa/LevelLoader.gd
+++ godot_project/tomi_testaa/LevelLoader.gd
@@ -9,10 +9,17 @@ var levels = [
 	preload("res://models/level2.glb"),
 	preload("res://models/level3.glb"),
 ]
+var props = [
+	preload("res://level0_propsit.tscn"),
+	preload("res://level1_propsit.tscn"),
+	preload("res://level2_propsit.tscn"),
+	null, # preload("res://level3_propsit.tscn")"),
+]
 var root_level = preload("res://tomi_testaa/hill_area.tscn")
 
 var root_scene_ref
 var level_scene_ref
+var props_scene_ref
 
 var load_next_level_timer = 0
 var wait_for_next_level = false
@@ -34,11 +41,15 @@ func load_level():
 		root_scene_ref.queue_free()
 	if is_instance_valid(level_scene_ref):
 		level_scene_ref.queue_free()
+	if is_instance_valid(props_scene_ref):
+		props_scene_ref.queue_free()
 	
 	level_scene_ref = levels[level_num].instantiate()
 	add_child(level_scene_ref)
 	root_scene_ref = root_level.instantiate()
 	add_child(root_scene_ref)
+	props_scene_ref = props[level_num].instantiate()
+	add_child(props_scene_ref)
 
 func next_level():
 	current_level += 1
