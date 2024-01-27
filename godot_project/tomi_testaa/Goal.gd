4c9c6d9ae2a1b50fca1d90ab5d9e1d8558726fc0
Tomi Kokkonen <tomi@valohai.com>
2024-01-27 20:12:37 +0200

    fix resource load

--- godot_project/tomi_testaa/Goal.gd
+++ godot_project/tomi_testaa/Goal.gd
@@ -1,9 +1,9 @@
 extends Area3D
 class_name GoalArea
 
-@onready var collision_shape_node:CollisionShape3D = $CollisionShape3D
-@onready var goal_visu:MeshInstance3D = $GoalDebugVisu
-@onready var the_game:= $".."
+@onready var collision_shape_node: CollisionShape3D = $CollisionShape3D
+@onready var goal_visu: MeshInstance3D = $GoalDebugVisu
+@onready var the_game = $".."
 
 func set_size_pos_rot(size: Vector3, pos: Vector3, rot: Vector3):
 	var shape: BoxShape3D = collision_shape_node.shape
