e4e2fa1d42aaa606de971007b697234a46ac9cb6
Raattis <raattis@gmail.com>
2024-01-27 22:24:44 +0200

    kalustus tool

--- godot_project/rikun_kansio/MaailmanKalustus.gd
+++ godot_project/rikun_kansio/MaailmanKalustus.gd
@@ -1,27 +1,34 @@
 @tool
 extends Node3D
 
+@export
+var generate_props := false
+
+@export
+var delete_props := false
+
+@export
+var random_seed := 0
 
 const props := [ "res://rompe_scenet/huussi.tscn", "res://rompe_scenet/kivi_1.tscn", "res://rompe_scenet/kivi_2.tscn", "res://rompe_scenet/kivi_3.tscn", "res://rompe_scenet/kivi_4.tscn", "res://rompe_scenet/kuusi.tscn", "res://rompe_scenet/lehtipuu.tscn", "res://rompe_scenet/manty.tscn" ]
 
 @onready var prosit = $"../Propsit"
 
-var random_seed := 0
-
-func _ready():
+func _generate():
 	seed(random_seed)
 	var space_state := get_world_3d().direct_space_state
 	
 	var shapes : Array[PackedScene] = []
 	
 	for prop in props:
-		#print(prop)
 		shapes.append(load(prop))
-		#print(shapes[-1])
 	
 	for i in range(5000):
 		var start := Vector3(randf_range(-1000, 1000), 3000, randf_range(-1000, 1000))
 		var shape_index := randi_range(0, len(shapes) - 1)
+		if shape_index == 0 and randi_range(0, 4) != 0:
+			continue # don't generate too many toilets
+		
 		var end := start + Vector3.DOWN * 6000
 		var query := PhysicsRayQueryParameters3D.create(start, end)
 		query.collision_mask = 1 # put all ropeable things on collision_layer 1
@@ -30,13 +37,14 @@ func _ready():
 			continue
 
 		var node := shapes[shape_index].instantiate()
-		node.global_position = result["position"]
 		prosit.add_child(node)
-		print(node.global_position)
-		
-	pass
+		node.global_position = result["position"] + Vector3.DOWN * 0.1
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if generate_props:
+		generate_props = false
+		_generate()
+	if delete_props:
+		delete_props = false
+		for child in prosit.get_children():
+			get_tree().queue_delete(child)
