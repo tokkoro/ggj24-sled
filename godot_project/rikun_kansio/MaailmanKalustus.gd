12f405baa590ab359dc4407379f00238258722f2
Raattis <raattis@gmail.com>
2024-01-28 13:32:21 +0200

    Coin trail

--- godot_project/rikun_kansio/MaailmanKalustus.gd
+++ godot_project/rikun_kansio/MaailmanKalustus.gd
@@ -11,74 +11,102 @@ var test_mode := false
 var delete_props := false
 
 @export
-var random_seed := 0
+var generate_coins := false
 
 @export
-var path_name := "level0_path"
+var delete_coins := false
+
+@export
+var random_seed := 0
 
 @export
 var intended_path_thickness := 20.0
 
+@export var intended_path : Path3D
+
+@export var coin_count : int = 30
+
 const props := [ "res://rompe_scenet/huussi.tscn", "res://rompe_scenet/kivi_1.tscn", "res://rompe_scenet/kivi_2.tscn", "res://rompe_scenet/kivi_3.tscn", "res://rompe_scenet/kivi_4.tscn", "res://rompe_scenet/kuusi.tscn", "res://rompe_scenet/lehtipuu.tscn", "res://rompe_scenet/manty.tscn" ]
 
-func _generate():
-	var intended_path : Path3D = null
+func _generate(do_coins:bool):
 	var prosit = $"../Propsit"
-	for path in [ $"../level2_path", $"../level0_path", $"../level1_path" ]:
-		if not path:
-			continue
-		if not path.name.contains(path_name):
-			continue
-		if intended_path:
-			print("Matches two paths: ", path_name, ", ", path, " != ", intended_path)
-			return
-		intended_path = path
+	var coins = $"../Coins"
 	var curve : Curve3D = intended_path.curve
 	seed(random_seed)
 	var space_state := get_world_3d().direct_space_state
 	
-	var shapes : Array[PackedScene] = []
-	
-	for prop in props:
-		shapes.append(load(prop))
-	
-	for i in range(5000):
-		var start := Vector3(randf_range(-1000, 1000), 3000, randf_range(-1000, 1000))
-		var shape_index := randi_range(0, len(shapes) - 1)
-		if shape_index == 0 and randi_range(0, 4) != 0:
-			continue # don't generate too many toilets
+	if not do_coins:
+		var shapes : Array[PackedScene] = []
 		
-		var end := start + Vector3.DOWN * 6000
-		var query := PhysicsRayQueryParameters3D.create(start, end)
-		query.collision_mask = 1 # put all ropeable things on collision_layer 1
-		var result := space_state.intersect_ray(query)
-		if not result:
-			continue
+		for prop in props:
+			shapes.append(load(prop))
+		
+		for i in range(5000):
+			var start := Vector3(randf_range(-1000, 1000), 3000, randf_range(-1000, 1000))
+			var shape_index := randi_range(0, len(shapes) - 1)
+			if shape_index == 0 and randi_range(0, 4) != 0:
+				continue # don't generate too many toilets
+			
+			var end := start + Vector3.DOWN * 6000
+			var query := PhysicsRayQueryParameters3D.create(start, end)
+			query.collision_mask = 1 # put all ropeable things on collision_layer 1
+			var result := space_state.intersect_ray(query)
+			if not result:
+				continue
 
-		var pos : Vector3 = result["position"] + Vector3.DOWN * 0.1
-		var closest_point := curve.get_closest_point(pos - intended_path.global_position) + intended_path.global_position
-		var dist_from_curve : float = (closest_point - pos).length()
-		if test_mode:
+			var pos : Vector3 = result["position"] + Vector3.DOWN * 0.1
+			var closest_point := curve.get_closest_point(pos - intended_path.global_position) + intended_path.global_position
+			var dist_from_curve : float = (closest_point - pos).length()
+			if test_mode:
+				var node := shapes[shape_index].instantiate()
+				prosit.add_child(node)
+				node.global_position = closest_point
+				print("a test, ", closest_point)
+				continue
+			print("not test")
+
+			if dist_from_curve < intended_path_thickness:
+				continue
+
 			var node := shapes[shape_index].instantiate()
 			prosit.add_child(node)
-			node.global_position = closest_point
-			print("a test, ", closest_point)
-			continue
-		print("not test")
+			node.global_position = pos
 
-		if dist_from_curve < intended_path_thickness:
-			continue
+	if do_coins:
+		var coin_prefab := load("res://rikun_kansio/coin.tscn")
+		var curve_length := curve.get_baked_length()
+		for i in range(coin_count):
+			var t : float = i * curve_length * 1.0 / coin_count
+			var start := curve.sample_baked(t) + intended_path.global_position + Vector3.UP * 30.0
+			
+			var end := start + Vector3.DOWN * 6000
+			var query := PhysicsRayQueryParameters3D.create(start, end)
+			query.collision_mask = 1 # put all ropeable things on collision_layer 1
+			var result := space_state.intersect_ray(query)
+			if not result:
+				continue
 
-		var node := shapes[shape_index].instantiate()
-		prosit.add_child(node)
-		node.global_position = pos
+			var pos : Vector3 = result["position"] + Vector3.DOWN * 0.1 + result["normal"] * 1.5
+			var node : Node3D = coin_prefab.instantiate()
+			prosit.add_child(node)
+			print(prosit, ", ", node, ", ", pos, ", ", t)
+			node.global_position = pos
 
 func _process(delta):
 	if generate_props:
 		generate_props = false
-		_generate()
+		_generate(false)
+
+	if generate_coins:
+		generate_coins = false
+		_generate(true)
 	if delete_props:
 		delete_props = false
 		var prosit = $"../Propsit"
 		for child in prosit.get_children():
+			get_tree().queue_delete(child)
+	if delete_coins:
+		delete_coins = false
+		var coinsit = $"../Coins"
+		for child in coinsit.get_children():
 			get_tree().queue_delete(child)
---
+++ godot_project/rikun_kansio/coin.gd
@@ -0,0 +1,43 @@
+extends Area3D
+
+@onready var gfx : MeshInstance3D = $gfx
+@onready var collect_sound : AudioStreamPlayer3D = $collect_sound
+
+const duration_ms := 1000.0
+var collected := false
+
+var delete_time := 1000000000000.0
+
+func _ready():
+	pass
+
+func _process(delta):
+	if not collected:
+		gfx.rotate_y(delta * 3.0)
+		return
+
+	if delete_time < Time.get_ticks_msec():
+		queue_free()
+		return
+
+	var t : float = max(0.0, 1.0 - (delete_time - Time.get_ticks_msec()) / duration_ms)
+	gfx.rotate_y(delta * lerp(3.0, 100.0, t))
+	translate(Vector3.UP * delta * lerp(0.0, 10.0, t))
+	scale = Vector3.ONE * min(1.0, lerp(10.0, 0.0, t))
+
+func collect():
+	if collected:
+		return
+	collected = true
+	delete_time = Time.get_ticks_msec() + duration_ms
+	collect_sound.play()
+
+func _on_body_entered(body):
+	if not body is Sled:
+		return
+	collect()
+
+func _on_area_entered(area):
+	if not area or not "Hook" in area.name:
+		return
+	collect()
